20487025763d890d71f22a112b63a6b26926f47a
Roshan996 <90492135+Roshan996@users.noreply.github.com>
2025-05-23 21:16:34 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,12 @@ SECRET_KEY = 'django-insecure-xkci3_l%93!vd@&qw0lzlyn@+(9oyul11vnf-55+y*m^8fo6!3
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
+STATIC_URL = '/static/'
 
+# This production code might break development mode, so we check whether we're in DEBUG mode
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Application definition
 
